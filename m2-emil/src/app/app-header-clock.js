f3cc1a3bd9042953d955648411d6c7abd6d39470
Emil Wall <emil.wall@consid.se>
2024-10-16 15:45:17 +0200

    Convert header clock to client component

--- m2-emil/src/app/app-header-clock.js
+++ m2-emil/src/app/app-header-clock.js
@@ -1,3 +1,13 @@
+'use client';
+import {useEffect, useState} from 'react';
+
 export default function AppHeaderClock({ isoDateString }) {
-  return <div>{new Date(isoDateString).toLocaleTimeString()}</div>;
+  const [date, setDate] = useState(new Date(isoDateString));
+  useEffect(() =>{
+    const interval = setInterval(() =>{
+      setDate(old => new Date(old.getTime() + 1000));
+    }, 1000);
+    return () => { clearInterval(interval); };
+  }, [])
+  return <div>{new Date(date).toLocaleTimeString()}</div>;
 }
